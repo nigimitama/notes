69ec3bc2d6716e807fd41018e74d06b2acae5b3b
nigimitama <nigimitama16@gmail.com>
2020-11-01 19:51:48 +0900

    wip / trying to use python, but not work

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-function build_book_ {
+
+function build_page {
     input_md_path=$1
-    # basename=${input_md_path##*/}
+    # dirname=${input_md_path%/*}
+    # cd $dirname
     output_html_path=${input_md_path//\.\//}  # remove ./
     output_html_path=${output_html_path//\//_}  # replace / with _
     output_html_path=${output_html_path//.md/.html}  # replace .md with .html
-    pandoc $input_md_path \
+    pandoc $basename \
         --output=$output_html_path \
         --standalone \
-        --self-contained \
         --mathjax \
         --highlight-style tango \
         --css="modules/style.css" \
@@ -20,27 +21,10 @@ function build_book_ {
 }
 
 
-function build_book {
-    input_md_path=$1
-    wd=$(pwd)
-    dirname=${input_md_path%/*}
-    cd $dirname
-    basename=${input_md_path##*/}
-    output_html_path=${basename//.md/.html}  # replace .md with .html
-    pandoc $basename \
-        --output=$output_html_path \
-        --standalone \
-        --self-contained \
-        --mathjax \
-        --highlight-style tango \
-        --css="$wd/modules/style.css" \
-        --template="$wd/modules/template.html" \
-        --title-prefix=盆暗の勉強メモ \
-        --toc
-}
-
 # reset
+# if [ -e ./docs/* ]; then
 rm -r ./docs/*
+# fi
 
 # css, jsのフォルダを移動する
 cp -r ./pandoc/modules ./docs
@@ -48,13 +32,28 @@ cp -r ./pandoc/modules ./docs
 # markdownファイルを移動する
 cp -r ./docs_raw/* ./docs
 
-# generate documents
+
+# assetsフォルダをhtmlと同じ階層に移動する
 cd ./docs
+assets_dirs=$(find -type d | grep .assets)
+# echo "assets_paths: $assets_paths"
+for assets_dir in $assets_dirs; do
+    dirname=${assets_dir##*/}
+    if [ ! -e $dirname ]; then
+        mkdir $dirname
+    fi
+    if [ "$assets_dir" != "./$dirname" ]; then
+        mv "$assets_dir" $dirname
+    fi
+done
+
+
+# generate documents: htmlをすべて一番浅い階層に生成する
 files=$(find | grep .md)
 echo "files: $files"
 for file in $files; do
     if [[ $file != *"README"* ]]; then
-        build_book $file
+        build_page $file
     fi
 done
 rm $files
