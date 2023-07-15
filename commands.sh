fd4882faf3647d037e1d1059f8865ae2ad37d8f2
mitama <nigimitama16@gmail.com>
2023-07-15 11:40:29 +0900

    update

--- commands.sh
+++ commands.sh
@@ -10,6 +10,10 @@ elif [ ${1} = 'bash' ]; then
     sudo docker-compose run workspace bash
 elif [ ${1} = 'jupyterlab' ]; then
     sudo docker-compose run --publish=8888:8888 workspace jupyter-lab --ip=0.0.0.0 --allow-root
+elif [ ${1} = 'push' ]; then
+	git add .
+	git commit -m "update"
+	git push
 else
     echo "Usage: bash ${0} [create|build|bash|jupyterlab]"
 fi
