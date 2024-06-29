3ea1aed08f1ae359212db3e604c1773b6fff4d3d
nigimitama <nigimitama16@gmail.com>
2024-06-29 11:26:45 +0900

    update

---
+++ search_not_in_toc.py
@@ -0,0 +1,43 @@
+# tocに入れ忘れている記事を探す
+from pprint import pprint
+from pathlib import Path
+import yaml
+import os
+
+
+list(Path().glob("*"))
+book_dir = Path("./book")
+os.chdir(book_dir)
+book_dir = Path(".")
+
+with open(book_dir / "_toc.yml", "r") as f:
+    toc = yaml.load(f, Loader=yaml.CLoader)
+
+
+part = toc["parts"][0]
+part["caption"]
+
+files = []
+for chapter in part["chapters"]:
+    files.append(chapter["file"])
+    if "sections" in chapter:
+        for section in chapter["sections"]:
+            files.append(section["file"])
+            if "sections" in section:
+                for section_ in section["sections"]:
+                    files.append(section_["file"])
+
+
+def valid_path(path: Path):
+    return (".ipynb_checkpoints" not in str(path)) \
+        and ("__pycache__" not in str(path)) \
+        and ("_build" not in str(path))
+
+
+parent_dir = Path(part["chapters"][0]["file"]).parent
+detected_files = [str(path).replace(".ipynb", "")
+                  for path in parent_dir.glob("*/*.ipynb") if valid_path(path)]
+
+set(detected_files) - set(files)
+
+pprint(toc)
