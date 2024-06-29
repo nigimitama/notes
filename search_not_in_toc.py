3cce9c15096c676595cb1ca86b5ec2d2ec1e912f
nigimitama <nigimitama16@gmail.com>
2024-06-29 11:29:50 +0900

    update

--- search_not_in_toc.py
+++ search_not_in_toc.py
@@ -14,30 +14,30 @@ with open(book_dir / "_toc.yml", "r") as f:
     toc = yaml.load(f, Loader=yaml.CLoader)
 
 
-part = toc["parts"][0]
-part["caption"]
+for part in toc["parts"]:
+    print(part["caption"], "-"*10)
 
-files = []
-for chapter in part["chapters"]:
-    files.append(chapter["file"])
-    if "sections" in chapter:
-        for section in chapter["sections"]:
-            files.append(section["file"])
-            if "sections" in section:
-                for section_ in section["sections"]:
-                    files.append(section_["file"])
+    files = []
+    for chapter in part["chapters"]:
+        files.append(chapter["file"])
+        if "sections" in chapter:
+            for section in chapter["sections"]:
+                files.append(section["file"])
+                if "sections" in section:
+                    for section_ in section["sections"]:
+                        files.append(section_["file"])
 
+    def valid_path(path: Path):
+        return (".ipynb_checkpoints" not in str(path)) \
+            and ("__pycache__" not in str(path)) \
+            and ("_build" not in str(path))
 
-def valid_path(path: Path):
-    return (".ipynb_checkpoints" not in str(path)) \
-        and ("__pycache__" not in str(path)) \
-        and ("_build" not in str(path))
+    parent_dir = Path(part["chapters"][0]["file"]).parent
+    detected_files = [str(path).replace(".ipynb", "")
+                      for path in parent_dir.glob("*/*.ipynb") if valid_path(path)]
 
+    files_not_in = set(detected_files) - set(files)
 
-parent_dir = Path(part["chapters"][0]["file"]).parent
-detected_files = [str(path).replace(".ipynb", "")
-                  for path in parent_dir.glob("*/*.ipynb") if valid_path(path)]
-
-set(detected_files) - set(files)
-
-pprint(toc)
+    print(f"{len(files_not_in)} files are detected")
+    for file in files_not_in:
+        print(file)
